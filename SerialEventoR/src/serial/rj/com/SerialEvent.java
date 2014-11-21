0ebe3a2de0134fd1498442eaeb0448e111c98a4d
usuario <usuario@192.168.1.101>
2014-11-21 08:32:37 -0430

    tratando de manejar puerto serial en java con rxtx

--- SerialEventoR/src/serial/rj/com/SerialEvent.java
+++ SerialEventoR/src/serial/rj/com/SerialEvent.java
@@ -5,17 +5,25 @@
  */
 package serial.rj.com;
 
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
 /**
  *
  * @author usuario
  */
-public class SerialEvent {
+public class SerialEvent implements SerialPortEventListener{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent spe) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
